75603703a25c017cc3f430a5a6623a6e7050d7c2
michelle <michelle@enfants.co>
2018-01-24 13:14:02 -0200

    Testing newDeck func

--- main.go
+++ main.go
@@ -1,17 +1,11 @@
 package main
 
 func main() {
-	//cards := newDeck()
-
-	//hand, remainingDeck := deal(cards, 5)
-
-	// fmt.Println(hand.toString())
-	// fmt.Println(remainingDeck.toString())
-
-	//hand.saveToFile("card-test")
-
 	cardsTwo := newDeckFromfile("card-test")
 	cardsTwo.print()
 	cardsTwo.shuffle()
 	cardsTwo.print()
+
+	cards := newDeck()
+	cards.print()
 }
